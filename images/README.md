47d7ae9b6b21cd6a30829eb24efe3a9f4f616064
kathirmeyyappan <kathirmey@gmail.com>
2024-07-30 17:54:39 -0700

    initial commit: refactored og html, moved script, and styled up for template

---
+++ images/README.md
@@ -0,0 +1,1 @@
+icons by [icons8](https://icons8.com/)
---
+++ script.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    document.getElementById(element).src = "images/" + element + "-white.png";
+}
+
+function gone(element) {
+    document.getElementById(element).src = "images/" + element + "-colored.png";
+}
